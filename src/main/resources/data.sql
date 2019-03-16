839929f1a37375cb733d9c303940dcd679cf7a35
jeyagandhi <gandigit@gmail.com>
2019-03-16 09:26:37 +0530

    DB2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into product
-values(10001,'Home Appliances', 'Samsung TV', 50000);
+values(10001,'Home Appliances', 'Samsung TV1', 50000);
 
 insert into product
-values(10002,'Home Appliances', 'Sony TV', 60000);
+values(10002,'Home Appliances', 'Sony TV2', 60000);
 
 insert into product
-values(10003,'Computer', 'Dell', 25000);
+values(10003,'Computer', 'Dell3', 25000);
