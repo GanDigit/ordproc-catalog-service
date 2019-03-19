3cdd5b570bd36cc186e3d9b98af8befda684fbb3
jeyagandhi <gandigit@gmail.com>
2019-03-19 10:38:03 +0530

    glusterfs

--- src/main/java/com/ord/catalogservice/CatalogserviceApplication.java
+++ src/main/java/com/ord/catalogservice/CatalogserviceApplication.java
@@ -2,12 +2,12 @@ package com.ord.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @SpringBootApplication
 public class CatalogserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogserviceApplication.class, args);
 	}
-
 }
